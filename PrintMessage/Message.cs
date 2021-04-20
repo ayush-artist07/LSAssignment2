57d54ce4d005eb343762d70be9080954270ccb25
ayush-artist07 <62583335+ayush-artist07@users.noreply.github.com>
2021-04-20 09:50:25 +0530

    Add files via upload

---
+++ PrintMessage/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintMessage
+{
+    public class Message
+    {
+        //method to print the message
+        public string PrintTheMessage(string message)
+        {
+            //Check for empty message
+            if (message.Count() == 0)
+            {
+                return "Enter valid string-";
+            }
+            
+            else
+                return "Hello " + message;
+
+        }
+    }
+}
